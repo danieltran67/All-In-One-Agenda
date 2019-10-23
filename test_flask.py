0c14ede8fbc9d71667767f4686d5adc6cd410c60
danieltran67 <danieltran150@gmail.com>
2019-10-23 16:47:21 -0700

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -1,19 +1,14 @@
-import os
-import tempfile
-
 import pytest
+from project.models import User
 
-from flask import Flask
 
-@pytest.fixture
-def client():
-    db_fd, Flask.app.config['DATABASE'] = tempfile.mkstemp()
-    Flask.app.config['TESTING'] = True
+@pytest.fixture(scope='module')
+def new_user():
+    user = User('patkennedy79@gmail.com', 'FlaskIsAwesome')
+    return user
 
-    with Flask.app.test_client() as client:
-        with Flask.app.app_context():
-            Flask.init_db()
-        yield client
-
-    os.close(db_fd)
-    os.unlink(Flask.app.config['DATABASE'])
+def test_new_user(new_user):
+    assert new_user.email == 'patkennedy79@gmail.com'
+    assert new_user.hashed_password != 'FlaskIsAwesome'
+    assert not new_user.authenticated
+    assert new_user.role == 'user'
