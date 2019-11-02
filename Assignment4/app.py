7ba46febc38c31180bfca7936d2f4fdc2245eb36
Daniel Tran <danieltran150@gmail.com>
2019-11-02 15:06:10 -0700

    SQL database works, but login verification is not working

---
+++ Assignment4/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("boilerplate.html")
+
+
+if __name__ == '__main__':
+    app.run()
