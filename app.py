af860aaa066335e4397762460c47fc631f2c7465
danieltran67 <danieltran150@gmail.com>
2019-11-04 20:49:44 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.urls import url_encode
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,8 +19,7 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 
 
-class User(db.Model):
-#class User(UserMixin, db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
