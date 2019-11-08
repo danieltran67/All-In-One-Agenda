9def421e145dcffd5b97a3b5ddd821584fc4856f
Antonio Magana <39139814+AntonioMagana@users.noreply.github.com>
2019-11-07 21:58:28 -0800

    Add files via upload

--- get_event_testcode.py
+++ get_event_testcode.py
@@ -1,9 +1,11 @@
+#Method 1
+
 var now = new Date();
 var twoHoursFromNow = new Date(now.getTime() + (2 * 60 * 60 * 1000));
 var events = CalendarApp.getDefaultCalendar().getEvents(now, twoHoursFromNow);
 Logger.log('Number of events: ' + events.length);
 
-##################################
+#Method 2
 
 event = service.events().get(calendarId='primary', eventId='eventId').execute()
 
